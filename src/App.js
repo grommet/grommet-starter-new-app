0cfee8cea4abc76171495f929eca5d00ae538aad
Jesse Olsen <jesse.olsen@hpe.com>
2020-06-09 21:31:58 -0600

    Reproduce DE1872.  Click Toggle button and notice on ie11 only, sidebar closes the wrong way, due to the 'submit' button in the layer.  If the type is 'button', everything is okay.

--- src/App.js
+++ src/App.js
@@ -2,104 +2,41 @@ import React, { Component } from 'react';
 import {
   Box,
   Button,
-  Collapsible,
-  Heading,
   Grommet,
   Layer,
-  ResponsiveContext,
 } from 'grommet';
-import { FormClose, Notification } from 'grommet-icons';
-
-const theme = {
-  global: {
-    colors: {
-      brand: '#228BE6',
-    },
-    font: {
-      family: 'Roboto',
-      size: '14px',
-      height: '20px',
-    },
-  },
-};
-
-const AppBar = (props) => (
-  <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='medium'
-    style={{ zIndex: '1' }}
-    {...props}
-  />
-);
 
 class App extends Component {
-  state = {
-    showSidebar: false,
-  }
+  state = { showSidebar: true }
   render() {
     const { showSidebar } = this.state;
     return (
-      <Grommet theme={theme} full>
-        <ResponsiveContext.Consumer>
-          {size => (
-            <Box fill>
-              <AppBar>
-                <Heading level='3' margin='none'>My App</Heading>
+      <Grommet>
+        <Box>
+          <Button
+            label='Toggle Sidebar'
+            type='submit'
+            onClick={() => this.setState({ showSidebar: !this.state.showSidebar })}
+          />
+        </Box>
+        <Box>
+          {showSidebar && (
+            <Layer position='right' full='vertical' modal={false}>
+              <Box
+                fill
+                background='light-2'
+                align='center'
+                justify='center'
+              >
+                Sidebar content
                 <Button
-                  icon={<Notification />}
-                  onClick={() => this.setState({ showSidebar: !this.state.showSidebar })}
+                  type='submit' /* DE1872 */
+                  label='button'
                 />
-              </AppBar>
-              <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-                <Box flex align='center' justify='center'>
-                  app body
-                </Box>
-                {(!showSidebar || size !== 'small') ? (
-                  <Collapsible direction="horizontal" open={showSidebar}>
-                    <Box
-                      flex
-                      width='medium'
-                      background='light-2'
-                      elevation='small'
-                      align='center'
-                      justify='center'
-                    >
-                      sidebar
-                    </Box>
-                  </Collapsible>
-                ): (
-                  <Layer>
-                    <Box
-                      background='light-2'
-                      tag='header'
-                      justify='end'
-                      align='center'
-                      direction='row'
-                    >
-                      <Button
-                        icon={<FormClose />}
-                        onClick={() => this.setState({ showSidebar: false })}
-                      />
-                    </Box>
-                    <Box
-                      fill
-                      background='light-2'
-                      align='center'
-                      justify='center'
-                    >
-                      sidebar
-                    </Box>
-                  </Layer>
-                )}
               </Box>
-            </Box>
+            </Layer>
           )}
-        </ResponsiveContext.Consumer>
+        </Box>
       </Grommet>
     );
   }
